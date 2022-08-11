0a201d40ec9dd71f8f2dff5df25595d44a086988
Karen1308 <karengarcia1308@gmail.com>
2022-08-10 22:00:15 -0300

    Proyectos de FrontEnd I

---
+++ Integrador/js/index.js
@@ -0,0 +1,62 @@
+const toggleBttn = document.getElementById('toggle-bttn')
+const navBarLink = document.getElementById('nav-bar-link')
+const navSlide = document.getElementById('nav-slide')
+const slide = document.getElementsByClassName("opinion");
+const dots = document.getElementsByClassName("dot");
+var slideIndex = 1;
+
+toggleBttn.addEventListener('click', () => {
+    navBarLink.classList.toggle('active');
+});
+
+// Funciones para el slide
+// Crear navegacion con los ciculos
+function navigationDots() {
+    for (i = 1; i <= slide.length; i++) {
+        elementBttn = `<button class="dot" id="dot${i}" onclick="slideNumber(${i})"></button>`;
+        navSlide.innerHTML += elementBttn;
+
+        if (i ==1){
+            dot1 = document.getElementById('dot'+i);
+            dot1.classList.toggle('dot-active');
+        }
+    };
+};
+
+function slideNumber(number) {
+    showSlide(slideIndex = number);
+};
+
+// Mostrando slide seleccionado en los botones
+function showSlide(number) {
+    if (number > slide.length) {
+        slideIndex = 1
+    };
+
+    if (number < 1) {
+        slideIndex = slide.length;
+    };
+
+    // Se ocultan todos los slide
+    for (i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    };
+
+    // Se oculta el que esta visible actualmente
+    const slideActive = document.getElementsByClassName('active-opinion')[0];
+    slideActive.classList.remove('active-opinion')
+    
+    // Se muestra el slide del boton seleccionado
+    slide[slideIndex - 1].classList.add('active-opinion');
+
+    // Elimino el que esta activo
+    dotActive = document.getElementsByClassName('dot-active')[0];
+    dotActive.classList.toggle('dot-active');
+
+    // Activamos el seleccionado
+    bttn = document.getElementById('dot' + number);
+    bttn.classList.toggle('dot-active');
+};
+
+// Llamo a la funciona navigation para los puntos
+navigationDots();
